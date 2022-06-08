11114fc1bb84fd9d273d11da14b8febafeb704b5
Brandon M Hunter <brandonmichaelhunter@gmail.com>
2022-06-08 08:25:26 -0400

    Created first django app

---
+++ Games/PythonGames/Jeopardy/static/js/index.js
@@ -0,0 +1,206 @@
+
+
+var mydata = JSON.parse(data);
+let htmlTable = '';
+let questionAnswers = [];
+let numberOfIncorrectAnswers = 0;
+var timeleft = 10;
+function ShowQuestion(QuestionID){
+    $("#Answer").hide();
+    $("#progressBar").hide();
+    let question = "";
+    let answer = "";
+    let value = "";
+    console.log("Question ID: " + QuestionID);
+    numberOfIncorrectAnswers = 0;
+    $("#QuestionID").val(QuestionID);
+    for(let a = 0; a <= questionAnswers.length; a++){
+        if(questionAnswers[a].ID == QuestionID){
+            question = questionAnswers[a].Question;
+            answer = questionAnswers[a].Answer;
+            value = questionAnswers[a].Value;
+            break;
+        }
+    }
+    $( "#dialog" ).addClass("QuestionDialog")
+    // questionAnswers.forEach(element => {
+    //     if(element.ID == QuestionID){
+            
+    //     }
+    // });
+    $("#Question").text(question);
+    $("#Answer").text(answer);
+    $("#Value").text(value);
+    //RunTimer();
+}
+function RunTimer(){
+    $("#progressBar").show();
+    
+    var downloadTimer = setInterval(function(){
+    if(timeleft <= 0){
+        $("#progressBar").removeClass("Started");
+        $("#progressBar").removeClass("GettingCloseDone");
+        $("#progressBar").removeClass("HalfWay");
+        $("#progressBar").addClass("Done")
+        clearInterval(downloadTimer);
+    }
+    else if (timeleft <= 7 && timeleft >= 5){
+        $("#progressBar").removeClass("Started");
+        $("#progressBar").removeClass("GettingCloseDone");
+        $("#progressBar").addClass("HalfWay");
+        $("#progressBar").removeClass("Done")
+    }
+    else if(timeleft <=5 ){
+        
+        $("#progressBar").removeClass("Started");
+        $("#progressBar").addClass("GettingCloseDone");
+        $("#progressBar").removeClass("HalfWay");
+        $("#progressBar").removeClass("Done");
+    }
+    else if (timeleft == 10){
+        $("#progressBar").addClass("Started");
+        $("#progressBar").removeClass("AlmostDone");
+        $("#progressBar").removeClass("GettingCloseDone");
+        $("#progressBar").removeClass("Done");
+    }
+
+    document.getElementById("progressBar").value = 10 - timeleft;
+    timeleft -= 1;
+    }, 1000);
+    //$("#progressBar").hide();
+}
+function LoadGame(){
+    htmlTable += '<table class="table table-bordered"><tr>';//'
+    for(let a = 0; a < mydata.length; a++){
+        console.log(mydata[a].category)
+        htmlTable+= '<td class="categoryHeader">'+mydata[a].category+'</td>'
+    }
+    htmlTable +='</tr>';
+    let counter = 4;
+    for(let a = 0; a < counter; a++){
+        htmlTable +='<tr>'
+        for(let b = 0; b < mydata.length; b++){
+            const qa = new Object();
+            qa.ID       = mydata[b].QuestionAnswer[a].ID;
+            qa.Question = mydata[b].QuestionAnswer[a].Question;
+            qa.Value    = mydata[b].QuestionAnswer[a].Value;
+            qa.Answer   = mydata[b].QuestionAnswer[a].Answer;
+            questionAnswers.push(qa);
+            htmlTable+= '<td class="ValuesRows"><button type = "button" id="'+mydata[b].QuestionAnswer[a].ID+'"  onclick="ShowQuestion(this.id);" class = "btn btn-link ValueBtn">'+mydata[b].QuestionAnswer[a].Value+'</button></td>' 
+        }
+        htmlTable+='</tr>';
+    }
+    //collect all the question answers into a dictionary.
+    htmlTable +='</table>'
+    $("#GameTable").append(htmlTable)
+}
+$(document).ready(function(){
+    LoadGame();
+    console.log(questionAnswers);
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        width: 750,
+        height: 400,
+        buttons: {
+            "Team A - Correct": function(){
+                var teamValue = parseInt($("#Team1Score").text())
+                teamValue += parseInt($("#Value").text());
+                $("#Team1Score").text(teamValue);
+                if(teamValue > 0){
+                    $("#Team1Score").addClass("PositiveValue");
+                    $("#Team1Score").removeClass("NegativeValue");
+                }
+                else {
+                    $("#Team1Score").addClass("NegativeValue");
+                    $("#Team1Score").removeClass("PositiveValue");
+                }
+                $("#Answer").show();
+                let controlID = $("#QuestionID").val();
+                $("#"+controlID+"").hide();
+                setTimeout(() => {  $( this ).dialog( "close" ); }, 2000);
+            },
+            "Team A - Incorrect": function(){
+                var teamValue = parseInt($("#Team1Score").text())
+                teamValue -= parseInt($("#Value").text());
+                $("#Team1Score").text(teamValue);
+                if(teamValue > 0){
+                    $("#Team1Score").addClass("PositiveValue");
+                    $("#Team1Score").removeClass("NegativeValue");
+                }
+                else {
+                    $("#Team1Score").addClass("NegativeValue");
+                    $("#Team1Score").removeClass("PositiveValue");
+                }
+                numberOfIncorrectAnswers = numberOfIncorrectAnswers +  1;
+                if(numberOfIncorrectAnswers == 2){
+                    timeleft = 0;
+                    console.log("Incorrect Team A")
+                    $("#Answer").show();
+                    let controlID = $("#QuestionID").val();
+                    $("#"+controlID+"").hide();
+                    setTimeout(() => {  $( this ).dialog( "close" ); }, 5000)
+                }
+                else {
+                    RunTimer();
+                }
+                
+                console.log("Number of incorrect answers:" + numberOfIncorrectAnswers);
+            },
+            "Team B - Correct": function(){
+                var teamValue = parseInt($("#Team2Score").text())
+                teamValue += parseInt($("#Value").text());
+                $("#Team2Score").text(teamValue);
+                if(teamValue > 0){
+                    $("#Team2Score").addClass("PositiveValue");
+                    $("#Team2Score").removeClass("NegativeValue");
+                }
+                else {
+                    $("#Team2Score").addClass("NegativeValue");
+                    $("#Team2Score").removeClass("PositiveValue");
+                }
+                $("#Answer").show();
+                let controlID = $("#QuestionID").val();
+                $("#"+controlID+"").hide();
+                setTimeout(() => {  $( this ).dialog( "close" ); }, 5000);
+            },
+            "Team B - Incorrect": function(){
+                var teamValue = parseInt($("#Team2Score").text())
+                teamValue -= parseInt($("#Value").text());
+                $("#Team2Score").text(teamValue);
+                if(teamValue > 0){
+                    $("#Team2Score").addClass("PositiveValue");
+                    $("#Team2Score").removeClass("NegativeValue");
+                }
+                else {
+                    $("#Team2Score").addClass("NegativeValue");
+                    $("#Team2Score").removeClass("PositiveValue");
+                }
+                numberOfIncorrectAnswers = numberOfIncorrectAnswers +  1;
+                if(numberOfIncorrectAnswers == 2){
+                    timeleft = 0;
+                    console.log("Incorrect Team B")
+                    $("#Answer").show();
+                    let controlID = $("#QuestionID").val();
+                    $("#"+controlID+"").hide();
+                    setTimeout(() => {  $( this ).dialog( "close" ); }, 5000);
+                }else {
+                    RunTimer();
+                }
+
+                console.log("Number of incorrect answers:" + numberOfIncorrectAnswers);
+            },
+            "Show Answer": function(){
+                $("#Answer").show();
+                let controlID = $("#QuestionID").val();
+                $("#"+controlID+"").hide();
+                setTimeout(() => {  $( this ).dialog( "close" ); }, 10000);
+            },
+            
+        },
+        show: { effect: "blind", duration: 1000 },
+        hide: { effect: "blind", duration: 1000 }
+      });
+      $( ".ValueBtn" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+      });
+})
---
+++ Games/PythonGames/Jeopardy/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
+
+# Create your views here.
+def index(request):
+    return render(request, "Jeopardy/index.html")
+
+
+# Create your views here.
